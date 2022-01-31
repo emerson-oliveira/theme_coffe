4642f90c1004fcab0613a81abee26fe1b5e805b2
Emerson Silva <emerson-ods@hotmail.com>
2022-01-31 00:27:50 -0300

    update taxonomia

--- assets/js/taxonomy.js
+++ assets/js/taxonomy.js
@@ -1,32 +1,50 @@
-jQuery(document).ready(($) => {
-  jQuery('#cmcMoreProducts').click((event) => {
-    event.preventDefault();
-    if (jQuery('#cmcSearch').val() == 1) {
-      jQuery('#loading').show();
-      $.ajax({
-        method: 'POST',
-        async: false,
-        url: ajax_object.ajax_url,
-        data: {
-          action: 'cmc_get_next_products', postType: jQuery('#cmcPostType').val(), taxonomy: jQuery('#cmcTaxonomy').val(), term: jQuery('#cmcTerm').val(), offset: jQuery('#cmcOffset').val(),
-        },
-        success(result) {
-          if (result != 2) {
-            jQuery('.list-products').append(result);
-            jQuery('#cmcOffset').val(parseInt(jQuery('#cmcOffset').val()) + 12);
-            jQuery('#loading').hide();
-          } else {
-            jQuery('#cmcSearch').val(2);
-            jQuery('#loading').hide();
-            jQuery('#cmcMoreProducts').hide();
-            jQuery('.cmc-message').show();
-          }
-        },
-        error(result) {
-          jQuery('.list-products').append('No se han podido cargar los productos');
-          jQuery('#loading').hide();
-        },
-      });
-    }
+document.addEventListener('DOMContentLoaded', () => {
+  // Variáveis
+  const cmcMoreProducts = document.getElementById('cmcMoreProducts');
+  const cmcSearch = document.getElementById('cmcSearch');
+  const cmcOffset = document.getElementById('cmcOffset');
+  const cmcPostType = document.getElementById('cmcPostType');
+  const cmcTaxonomy = document.getElementById('cmcTaxonomy');
+  const cmcTerm = document.getElementById('cmcTerm');
+  const loading = document.getElementById('loading');
+  const cmcMessage = document.getElementById('cmc-message');
+
+  // Eventos
+  cmcPostType.addEventListener('change', () => {
+    cmcMoreProducts.addEventListener('click', (event) => {
+      event.preventDefault();
+      if (cmcSearch.value === 1) {
+        loading.style.display = 'block';
+        fetch(ajax_object.ajax_url, {
+          method: 'POST',
+          body: new URLSearchParams({
+            action: 'cmc_get_next_products',
+            postType: cmcPostType.value,
+            taxonomy: cmcTaxonomy.value,
+            term: cmcTerm.value,
+            offset: cmcOffset.value,
+          }),
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            if (data !== 2) {
+              document.querySelector('.list-products').insertAdjacentHTML('beforeend', data);
+              cmcOffset.value = parseInt(cmcOffset.value, 10) + 12;
+              loading.style.display = 'none';
+            } else {
+              cmcSearch.value = 2;
+              loading.style.display = 'none';
+              cmcMoreProducts.style.display = 'none';
+              cmcMessage.style.display = 'block';
+            }
+          })
+          .catch((error) => {
+            console.log(error);
+            loading.style.display = 'none';
+            document.querySelector('.list-products')
+              .insertAdjacentHTML('beforeend', 'No se han podido cargar los productos');
+          });
+      }
+    });
   });
 });
